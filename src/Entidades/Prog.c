1079d912dfac4aa0956664d4c628453fcce39c01
Chandre <andrebt.2149@gmail.com>
2024-04-26 16:05:50 +0100

    Ochestrator very very early start

--- src/Entidades/Prog.c
+++ src/Entidades/Prog.c
@@ -27,23 +27,43 @@ Prog * create_Prog(char * path_to_program, short amount_args, char ** args){
 
     return x;
 }
-void execute_Prog(Prog * x){
+void execute_Prog(Prog * x , char * outputpath){
 
     pid_t pid = fork();
     int status;
 
-    if(pid == -1){
+    if(pid == -1)
+    {
         perror("O garfo não garfou");
         exit(EXIT_FAILURE);
     }
     
     else if(pid == 0)
-    execvp(x->path_to_program, x->args);
+    {
+                          // Ola fred mudei isto um coto... acho que n tirei nada....
+        int file = open (outputpath , O_WRONLY) ;
+        dup2 (file , 1) ; // eu nao sei como e que depois de usar isto desfaco os efeitos
+                          // e nao sei o que e que isto afeta mas supostamente o output
+                          // do que executamos tera de ir para um ficheiro (paco disse txt btw)
+                          //suponho que se tenha de fazer uma condicao para quando for com varios
+                          //comandos seguidos porque ai acho que nao deve ir para um file mas
+                          // sim para um pipe dos nao named
+
+                          //ass(:) Chandre
+
+                          // (.) (.)
+                          //    -
     
+
+        execvp(x->path_to_program, x->args) ;
+    }
+
     else
-    waitpid(pid, &status, 0);
+    waitpid(pid, &status, 0) ;
 
 }
+
+
 void destroy_Prog(Prog * x){
     free(x->path_to_program);
     for(int i = 0; i<x->amount_args; i++)
